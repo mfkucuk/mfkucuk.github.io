21997955d036d7376791a617aa623b9bbe123413
mfkucuk <mfkucuk555@gmail.com>
2021-01-21 20:13:26 +0300

    really small bug

--- hots-dps-calculator/js/controllers/MainController.js
+++ hots-dps-calculator/js/controllers/MainController.js
@@ -6,8 +6,8 @@ app.controller('heroesCtrl', function($scope, $routeParams) {
 	$scope.heroName = $routeParams.hero;
 	$scope.hero = heroByName( $scope.heroName);
 	$scope.level = 1;
-	$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level - 1));
+	$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level-1));
 	$scope.calculateDps = function() {
-		$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level - 1));
+		$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level-1));
 	}
 });
