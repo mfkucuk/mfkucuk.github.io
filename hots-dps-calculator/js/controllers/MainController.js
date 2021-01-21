e31b5a99e968c4a7b634dce21222fe5636a81930
mfkucuk <mfkucuk555@gmail.com>
2021-01-22 01:48:41 +0300

    added enemy armor

--- hots-dps-calculator/js/controllers/MainController.js
+++ hots-dps-calculator/js/controllers/MainController.js
@@ -6,8 +6,9 @@ app.controller('heroesCtrl', function($scope, $routeParams) {
 	$scope.heroName = $routeParams.hero;
 	$scope.hero = heroByName( $scope.heroName);
 	$scope.level = 1;
-	$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level-1));
+	$scope.enemyArmor = 0;
+	$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level-1) * ((100 - $scope.enemyArmor) / 100 ));
 	$scope.calculateDps = function() {
-		$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level-1));
+		$scope.dps = Math.floor($scope.hero.attackDamage * $scope.hero.attackSpeed * Math.pow(1.04, $scope.level-1) * ((100 - $scope.enemyArmor) / 100 ));
 	}
 });
